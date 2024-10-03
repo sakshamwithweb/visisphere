2fbb0fcc33d6393d6490c20fd5e6a9576ca842df
Saksham Goswami <websaksham39@gmail.com>
2024-10-03 07:34:02 +0530

    chapter page

--- app/chapter/[slug]/page.js
+++ app/chapter/[slug]/page.js
@@ -1,6 +1,6 @@
 "use client"
 import dynamic from 'next/dynamic';
-
+// Here using dynamic I am rediracting course components in /chapter/[courseName] sccording to the course name
 const Page = ({ params }) => {
     const DynamicComponent = dynamic(() => import(`@/components/Chapters/${params.slug}`), {
         ssr: false
