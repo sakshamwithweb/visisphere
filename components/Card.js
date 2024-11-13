521f467071b88ffe803b62ce268e8eae9698404f
Saksham Goswami <websaksham39@gmail.com>
2024-11-13 15:57:50 +0530

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -1,25 +1,28 @@
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
+
 // It is a card for showing the course
 const Card = ({ imageSrc, title, description }) => {
-    // The props are comming from Home page
+    // The props are coming from the Home page
     return (
-        <div className="max-w-sm bg-white shadow-md rounded-lg overflow-hidden border-2">
-            <div className="h-0 pb-80 relative">
-                <img
-                    src={imageSrc}
-                    alt={title}
-                    className="absolute inset-0 w-full h-full object-cover"
-                />
-            </div>
-            <div className="p-4">
-                <h2 className="text-xl font-semibold mb-2">
-                    <Link href={`/chapter/${title}`}>{title}</Link>
-                </h2>
-                <p className="text-gray-600">{description}</p>
-            </div>
-        </div>
+        <Link href={`/chapter/${title}`}>
+            <a className="max-w-sm bg-white shadow-md rounded-lg overflow-hidden border-2 block">
+                <div className="h-0 pb-80 relative">
+                    <img
+                        src={imageSrc}
+                        alt={title}
+                        className="absolute inset-0 w-full h-full object-cover"
+                    />
+                </div>
+                <div className="p-4">
+                    <h2 className="text-xl font-semibold mb-2">
+                        {title}
+                    </h2>
+                    <p className="text-gray-600">{description}</p>
+                </div>
+            </a>
+        </Link>
     );
 }
 
-export default Card
+export default Card;
